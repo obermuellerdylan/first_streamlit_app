182006266c9712202f91aa4bc987c65ee2ffff4d
Dylan Obermueller <60851725+obermuellerdylan@users.noreply.github.com>
2022-09-19 12:53:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+# format the json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# print json in table
+streamlit.dataframe(fruityvice_normalized)
